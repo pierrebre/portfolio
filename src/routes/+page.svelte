827920dd2b825e1df3b8c2be90294651ba0c44fa
Pierre Barbé <pierrebarbe0@gmail.com>
2024-06-25 17:37:47 +0200

    todo navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,22 @@
 <script>
 	import '../app.css';
+	import { Download } from 'lucide-svelte';
 </script>
 
-<h1 class="text-3xl font-bold underline">Pierre Barbé</h1>
-
+<div class="container mx-auto px-4 mt-4">
+	<nav class="flex justify-between">
+		<p class="text-xl font-bold">Pierre Barbé</p>
+		<div>
+			<a href="/#">About Me</a>
+			<a href="/#">Skills</a>
+			<a href="/#">Projects</a>
+			<a href="/#">Contact Me</a>
+		</div>
+		<button class="bg-black text-white py-2 px-5 rounded flex"
+			>Resume<Download color="#ffffff" /></button
+		>
+	</nav>
+	<section>
+			<p>Hello I'am Pierre BARBÉ</p>
+	</section>
+</div>
