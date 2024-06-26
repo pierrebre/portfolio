5c34cfc474ae7d25e3ff99452cababac5a4c1b19
Pierre Barbé <pierrebarbe0@gmail.com>
2024-06-26 13:18:07 +0200

    Todo: navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,33 @@
 </script>
 
 <div class="container mx-auto px-4 mt-4">
-	<nav class="flex justify-between">
+	<nav class="flex justify-between align-baseline">
 		<p class="text-xl font-bold">Pierre Barbé</p>
-		<div>
-			<a href="/#">About Me</a>
-			<a href="/#">Skills</a>
-			<a href="/#">Projects</a>
-			<a href="/#">Contact Me</a>
-		</div>
+		<ul class="flex">
+			<li><a class="mr-8" href="/#">About Me</a></li>
+			<li><a class="mr-8" href="/#">Skills</a></li>
+			<li><a class="mr-8" href="/#">Projects</a></li>
+			<li><a class="mr-8" href="/#">Contact Me</a></li>
+		</ul>
 		<button class="bg-black text-white py-2 px-5 rounded flex"
-			>Resume<Download color="#ffffff" /></button
+			>Resume<Download class="ml-2 mt-[0.15rem]" size={18} color="#ffffff" /></button
 		>
 	</nav>
-	<section>
-			<p>Hello I'am Pierre BARBÉ</p>
+	<section class="mt-16">
+		<div>
+			<p>
+				Hello I'am <span>Pierre BARBÉ.</span><br />
+				<span>Web</span> Developper<br />
+				Based In <span>Montreal.</span>
+			</p>
+		</div>
+		<div class="mt-8">
+			<p class="texte text-[#8e8b8c]">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam perferendis pariatur
+				consectetur soluta, eum explicabo nesciunt nam in incidunt vitae. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Quisquam perferendis pariatur consectetur soluta, eum
+				explicabo nesciunt nam in incidunt vitae.
+			</p>
+		</div>
 	</section>
 </div>
