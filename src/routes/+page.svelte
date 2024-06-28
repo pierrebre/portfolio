815b0c7f0b84d1b6f73d5f6830c6a877c24a49aa
Pierre Barbé <pierrebarbe0@gmail.com>
2024-06-28 21:35:45 +0200

    TODO pf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,24 @@
 <script>
 	import '../app.css';
-	import { Download } from 'lucide-svelte';
+	import { Download, Github, Linkedin } from 'lucide-svelte';
+	import { Button } from "$lib/components/ui/button";
 </script>
 
 <div class="container mx-auto px-4 mt-4">
 	<nav class="flex lg:flex-row flex-col justify-between align-baseline">
 		<p class="text-xl font-bold">Pierre Barbé</p>
-		<ul class="flex">
-			<li><a class="mr-8" href="/#">About Me</a></li>
-			<li><a class="mr-8" href="/#">Skills</a></li>
-			<li><a class="mr-8" href="/#">Projects</a></li>
-			<li><a class="mr-8" href="/#">Contact Me</a></li>
-		</ul>
-		<button class="bg-black text-white py-2 px-5 rounded flex"
-			>Resume<Download class="ml-2 mt-[0.15rem]" size={18} color="#ffffff" /></button
-		>
+			<ul class="flex lg:flex-row flex-col my-4">
+				<li><a class="mr-8" href="/#">About Me</a></li>
+				<li><a class="mr-8" href="/#">Skills</a></li>
+				<li><a class="mr-8" href="/#">Projects</a></li>
+				<li><a class="mr-8" href="/#">Contact Me</a></li>
+			</ul>
+			<div>
+				<Button>
+					<Download class="mr-2 h-4 w-4" />
+					Resume
+				</Button>
+			</div>
 	</nav>
 	<section class="mt-16">
 		<div>
@@ -31,6 +35,14 @@
 				consectetur adipisicing elit. Quisquam perferendis pariatur consectetur soluta, eum
 				explicabo nesciunt nam in incidunt vitae.
 			</p>
+		</div>
+		<div class="flex mt-16">
+			<a  href="/" class="mr-8 border-solid border-2 border-black p-4 rounded hover:bg-black">
+				<Github class='h-4 w-4' />
+			</a>
+			<a href="/" class="mr-8 border-solid border-2 border-black p-4 rounded">
+				<Linkedin class='h-4 w-4' />
+			</a>
 		</div>
 	</section>
 </div>
