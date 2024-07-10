9e0f657c1d55bc7a9c455e0a383e39b0977469e6
Pierre Barbé <pierrebarbe0@gmail.com>
2024-07-10 17:52:04 +0200

    add: sveltekit-i18n and start of translations

---
+++ src/lib/translation/index.ts
@@ -0,0 +1,36 @@
+import i18n from 'sveltekit-i18n';
+import lang from './lang.json';
+
+/** @type {import('sveltekit-i18n').Config} */
+export const config = {
+	translations: {
+		en: { lang },
+		fr: { lang }
+	},
+	loaders: [
+		{
+			locale: 'fr',
+			key: 'home',
+			loader: async () => (await import('./fr/home.json')).default
+		},
+		{
+			locale: 'fr',
+			key: 'skills',
+			loader: async () => (await import('./fr/skills.json')).default
+		},
+		{
+			locale: 'en',
+			key: 'home',
+			loader: async () => (await import('./en/home.json')).default
+		},
+		{
+			locale: 'en',
+			key: 'skills',
+			loader: async () => (await import('./en/skills.json')).default
+		}
+	]
+};
+
+export const { t, loading, locales, locale, loadTranslations } = new i18n(config);
+
+loading.subscribe(($loading) => $loading && console.log('Loading translations...'));
