0cf208428c10bdf79bef3db488db285478e97351
Pierre Barbé <pierrebarbe0@gmail.com>
2024-07-13 13:42:56 +0200

    add: navbar component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
 	import { t, locales, locale } from '$lib/translation';
 	import * as Select from '$lib/components/ui/select';
+	import Navbar from '$lib/components/navbar/navbar.svelte';
 
 	function handleSelectedChange(value: any) {
 		$locale = value;
 	}
+
+	const links = [
+		{ href: '/#skills', name: $t('home.titleSection1') },
+		{ href: '/#experiences', name: $t('home.titleSection2') },
+		{ href: '/#contact', name: 'Contact' },
+		{ href: '/blog', name: 'Blog' }
+	];
+
+	let isVisible = false;
 </script>
 
 <!--<div class=" left-0 top-0 z-10 m-0 p-0 w-full h-full">
@@ -20,5 +30,8 @@
 	</Select.Root>
 </div>
 -->
+<div class="container mx-auto mt-4">
+	<Navbar {isVisible} {links} />
+</div>
 
 <slot />
